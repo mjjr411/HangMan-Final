f0e9a30f1525732e121130f979779f9fcb14093f
samuelbadasa <samuelbadasa@gmail.com>
2024-11-06 14:20:07 -0600

    some message

---
+++ impoort.py
@@ -0,0 +1,15 @@
+import random
+
+MOVIE= [
+    "Intersteller",
+    "The Martian"
+    "Star wars"
+    "Avengers"
+]
+
+SPORT= [
+    "Football"
+    "Point Guard"
+    "Quaterback"
+    "Soccer"
+]
